8f6d44c891d1da02372ae6777f892d1a1529bdaa
EstebanC99 <estebancarignani99gmail.com>
2020-10-21 20:03:45 -0300

    Comienzo ejercicio 1

---
+++ Ejercicio1/Ejercicio1/Vehiculo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    public abstract class Vehiculo
+    {
+        #region Atributos
+        private int cantPasajeros;
+        #endregion
+
+
+
+    }
+}
