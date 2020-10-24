ba3dfc6ce85195750205cdaa2dc8c3a2a8cfeba2
estebancarignani99@gmail.com <estebancarignani99@gmail.com>
2020-10-23 21:09:46 -0300

    Empezando Excepciones

---
+++ DemoDivisionPorCero/DemoDivisionPorCero/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoDivisionPorCero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int valor1 = int.Parse(Console.ReadLine());
+                int valor2 = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+
+            }
+
+        }
+    }
+}
